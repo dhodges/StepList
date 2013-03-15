5eaf29194b2a4cfb16a138746f2967a5be910942
david <david.hodges@lonelyplanet.com.au>
2013-03-15 21:04:55 +1100

    sort steps alphabetically

--- core/stepper.py
+++ core/stepper.py
@@ -34,7 +34,7 @@ class Stepper(object):
     steps = []
     for file in self.step_definition_files():
       steps.extend(self.steps_defined_in(file))
-    return sorted(steps)
+    return sorted(steps, lambda x, y: cmp(x.lower(), y.lower()))
 
   def snippet(self, text):
     self.index = 0
